a723038c134c6b2ef030bf8269a34dc7f389a040
shashimalcse <shashimalsenarath.17@cse.mrt.ac.lk>
2020-03-20 22:21:21 +0530

    enterpassword logic done

--- app/src/main/java/com/example/myapplication/EnterPasswordActivity.java
+++ app/src/main/java/com/example/myapplication/EnterPasswordActivity.java
@@ -10,26 +10,39 @@ import android.widget.TextView;
 import com.goodiebag.pinview.Pinview;
 
 public class EnterPasswordActivity extends AppCompatActivity {
-
+    private String PasswordSize;
+    private String Color;
+    private Pinview pin;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_enterpassword);
 
-        TextView size = (TextView) findViewById(R.id.password_size);
+
         TextView color = (TextView) findViewById(R.id.colorview);
-        Pinview pin = (Pinview) findViewById(R.id.pinview1);
+        pin = (Pinview) findViewById(R.id.pinview1);
 
 
         Intent intent = getIntent();
-        String PasswordSize = intent.getStringExtra("PasswordSize");
-        String Color = intent.getStringExtra("Color");
+        PasswordSize = intent.getStringExtra("PasswordSize");
+        Color = intent.getStringExtra("Color");
         Log.d("s",PasswordSize);
         pin.setPinLength(Integer.parseInt(PasswordSize));
         if(Integer.parseInt(PasswordSize)==8){
             pin.setSplitWidth(10);
         }
-        size.setText(PasswordSize);
-        color.setText(Color);
+
+        color.setText("Enter " + PasswordSize +" character password");
+
+
+        pin.setPinViewEventListener(new Pinview.PinViewEventListener() {
+            @Override
+            public void onDataEntered(Pinview pinview, boolean fromUser) {
+                Intent i = new Intent(EnterPasswordActivity.this,ConfirmPasswordActivity.class);
+                i.putExtra("PasswordSize",PasswordSize);
+                i.putExtra("Color",Color);
+                i.putExtra("Password",pin.getValue());
+            }
+        });
     }
 }
