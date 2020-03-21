cb7bc87f051f8ef7868bbebd55b54596950f833d
shashimalcse <shashimalsenarath.17@cse.mrt.ac.lk>
2020-03-22 01:20:47 +0530

    grid view adapter done

--- app/src/main/java/com/example/myapplication/ImageGridActivity.java
+++ app/src/main/java/com/example/myapplication/ImageGridActivity.java
@@ -3,12 +3,21 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.GridView;
+import android.widget.ImageView;
 
 public class ImageGridActivity extends AppCompatActivity {
+
+    GridView gridView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_image_grid);
+        gridView = (GridView) findViewById(R.id.gridview);
+
+        ImageAdapter imageAdapter = new ImageAdapter(this,8);
+        gridView.setAdapter(imageAdapter);
+
     }
 }
